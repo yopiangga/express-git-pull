bce08e257701af834285b6d0a3dad3ba6c109373
yopiangga <yopigambyok@gmail.com>
2024-06-04 15:25:35 +0700

    auto update

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const app = express();
 // Replace with your GitHub repository URL
 const gitUrl = "https://github.com/yopiangga/express-git-pull.git";
 
-app.get("/update-github", async (req, res) => {
-  try {
-    const git = simpleGit(process.cwd()); // Initialize Git instance in current directory
+const git = simpleGit(process.cwd());
 
-    // Pull the latest changes from the remote repository
-    await git.pull(gitUrl);
+git.pull(gitUrl);
 
-    console.log("Repository updated successfully! :)");
-    res.send("Repository updated successfully!");
-  } catch (error) {
-    console.error("Error updating repository:", error);
-    res.status(500).send("Error updating repository");
-  }
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+// app.get("/update-github", async (req, res) => {
+//   try {
+//     const git = simpleGit(process.cwd());
+
+//     await git.pull(gitUrl);
+
+//     console.log("Repository updated successfully! :)");
+//     res.send("Repository updated successfully!");
+//   } catch (error) {
+//     console.error("Error updating repository:", error);
+//     res.status(500).send("Error updating repository");
+//   }
+// });
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
